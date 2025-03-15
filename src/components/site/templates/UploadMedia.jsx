1d2f8fcac08b8fe3b9d27f6ee94b710174b3d72e
wati-sender <watisender@gmail.com>
2025-03-15 18:16:07 +0530

    Update UploadMedia.jsx

--- src/components/site/templates/UploadMedia.jsx
+++ src/components/site/templates/UploadMedia.jsx
@@ -1,10 +1,11 @@
-import { InboxOutlined, UploadOutlined } from '@ant-design/icons'
+import { InboxOutlined, LoadingOutlined, UploadOutlined } from '@ant-design/icons'
 import { Button, Card } from 'antd'
 import Dragger from 'antd/es/upload/Dragger'
 import React from 'react'
 
 const UploadMedia = ({
     url,
+    loading,
     handleUpload
 }) => {
 
@@ -17,20 +18,19 @@ const UploadMedia = ({
                 customRequest={handleUpload}>
                 {url ? <img src={url} alt="" height={165} /> : <>
                     <Card>
-                        <Button type="primary" icon={<UploadOutlined />}>
-                            Upload Media
-                        </Button>
-                        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-                        <p className="ant-upload-hint">
-                            Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-                            banned files.
-                        </p>
+                        {loading ? (<LoadingOutlined />) : (<>
+                            <Button type="primary" icon={<UploadOutlined />}>
+                                Upload Media
+                            </Button>
+                            <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                            <p className="ant-upload-hint">
+                                Support for a single or bulk upload. Strictly prohibited from uploading company data or other
+                                banned files.
+                            </p></>)}
                     </Card>
                 </>}
-
-
-            </Dragger>
-        </div>
+            </Dragger >
+        </div >
     )
 }
 
